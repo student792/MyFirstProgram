3c6fadf0bf0abdb33011e6f6460f958c7e4682ba
student792 <78609760+student792@users.noreply.github.com>
2022-06-02 19:18:36 +0100

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,14 @@
+/*
+ *MyFirstProgram.java
+ *@author U.Aniekwena
+ *03/11/2021
+ */
+
+
+public class MyFirstProgram{
+	public static void main(String args[]){
+		System.out.println("I love Java");
+		System.out.println("I love programming");
+
+	}
+}
